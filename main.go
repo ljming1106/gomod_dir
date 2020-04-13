90baaa4c3cde9abd0f51353ce8d7e7926d4b6dea
JL <f>
2020-04-13 23:37:54 +0800

    worker模式并发获取数据

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-//"gomod_dir/concurrent"
-"gomod_dir/practice"
+"gomod_dir/concurrent"
+//"gomod_dir/practice"
 )
 
 func main() {
 	///*测试并发读数据*/
-	//concurrent.GetData()
+	concurrent.GetData()
 	//
 	///*测试并发程序性能*/
 	//concurrent.Deal()
@@ -16,5 +16,5 @@ func main() {
 	//practice.TestSlice()
 
 	/*channel任务池*/
-	practice.Exec_channel_workpool()
+	//practice.Exec_channel_workpool()
 }
